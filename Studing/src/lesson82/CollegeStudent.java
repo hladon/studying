677eda1701c4975a150e781a5640b878d066d1b2
Vitaliy <hladon@meta.ua>
2018-10-01 23:00:49 +0300

    Delete wrong

---
+++ Studing/src/lesson82/CollegeStudent.java
@@ -0,0 +1,18 @@
+package lesson82;
+
+public class CollegeStudent extends Student{
+	String collegeName;
+	int rating;
+	long id;
+	public CollegeStudent(String firstName, String lastName, int groupe, Course[] coursesTaken, String collegeName,
+			int rating, long id) {
+		super(firstName, lastName, groupe, coursesTaken);
+		this.collegeName = collegeName;
+		this.rating = rating;
+		this.id = id;
+	}
+	public CollegeStudent(String firstName, String lastName, int groupe, Course[] coursesTaken) {
+		super(firstName, lastName, groupe, coursesTaken);
+	}
+	
+}
